da69d5cc6ec39a14ad20f369dc2178bde932af48
Massimiliano Rizzuto <massimiliano.rizzuto87@gmail.com>
2020-11-16 17:15:14 +0100

    refactor: create more utils function for click handler function
    Create new functions to store calculation data, solve single operation and log management.

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,10 @@
+/**
+ * Create a single element HTML
+ * @param {String} tag - Name of the tag HTML to create.
+ * @param {String[]} classes - Each elements of this array represents a single class.
+ * @param {String} content - The content inside the tag.
+ * @param {Array} attribute - Each object of this array represents a single attribute of tag. The object must have this structure: {name: 'attr', value: 'attr value'}.
+ */
 function createElementHtml(tag, classes = [], content = '', attribute = []) {
   const el = document.createElement(tag);
   if (classes.length > 0) {
@@ -12,6 +19,25 @@ function createElementHtml(tag, classes = [], content = '', attribute = []) {
 
 function cleanHTML(tag) {
   tag.innerHTML = '';
+}
+
+function storeCalculationData(data, value, operandType) {
+  switch (operandType) {
+    case 'first':
+      // Store first operand
+      data.firstOperand.push(value);
+      data.display = data.firstOperand.join('');
+      break;
+    case 'second':
+      // Store second operand
+      data.secondOperand.push(value);
+      data.display = data.secondOperand.join('');
+      break;
+    case 'operation':
+      // Store sign of the operation
+      data.operation = value;
+      break;
+  }
 }
 
 function calculationResult(firstOp, op, secondOp) {
@@ -33,26 +59,51 @@ function calculationResult(firstOp, op, secondOp) {
   return result;
 }
 
+function solveSingleOperation(first, operation, second) {
+  const firstOp = parseFloat(first);
+  const secondOp = parseFloat(second);
+  const result = calculationResult(firstOp, operation, secondOp);
+  return result;
+}
+
+function log(logData, value, actionType) {
+  switch (actionType) {
+    case 'store':
+      logData.singleOpLog.push(value);
+      break;
+    case 'storeHistory':
+      logData.logHistory.push(value);
+      break;
+    case 'clean':
+      logData.singleOpLog = value;
+      break;
+    case 'cleanHistory':
+      logData.logHistory = value;
+      break;
+  }
+}
+
 function cleanCalculationData(data) {
   data.firstOperand = [];
   data.operation = null;
   data.secondOperand = [];
-  data.display = '';
 }
 
 function scrollToLog() {
   const displayLog = document.getElementById('log');
-    window.scroll({
-      top: displayLog.offsetTop,
-      left: 0,
-      behavior: 'smooth',
-    });
+  window.scroll({
+    top: displayLog.offsetTop,
+    left: 0,
+    behavior: 'smooth',
+  });
 }
 
 export {
   createElementHtml,
   cleanHTML,
-  calculationResult,
+  storeCalculationData,
+  solveSingleOperation,
+  log,
   cleanCalculationData,
-  scrollToLog
+  scrollToLog,
 };
